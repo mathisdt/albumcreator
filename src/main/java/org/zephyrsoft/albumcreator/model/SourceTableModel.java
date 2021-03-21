348bf905e4a99cabeba95ec5e7afccaf1c83e6ed
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2021-03-21 15:41:31 +0100

    use ArrayList in favor of LinkedList

--- src/main/java/org/zephyrsoft/albumcreator/model/SourceTableModel.java
+++ src/main/java/org/zephyrsoft/albumcreator/model/SourceTableModel.java
@@ -1,8 +1,8 @@
 package org.zephyrsoft.albumcreator.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.event.TableModelEvent;
@@ -10,55 +10,55 @@ import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
 
 public class SourceTableModel implements TableModel {
-	
+
 	private static final List<String> columns = Arrays.asList("Artist", "Title");
-	
-	private List<SourceFile> files = new LinkedList<>();
-	private List<TableModelListener> listeners = new LinkedList<>();
-	
+
+	private List<SourceFile> files = new ArrayList<>();
+	private List<TableModelListener> listeners = new ArrayList<>();
+
 	public void addAll(List<SourceFile> sourceFiles) {
 		files.addAll(sourceFiles);
 		listeners.forEach(listener -> listener.tableChanged(new TableModelEvent(this)));
 	}
-	
+
 	public void clear() {
 		files.clear();
 		listeners.forEach(listener -> listener.tableChanged(new TableModelEvent(this)));
 	}
-	
+
 	public List<SourceFile> getFiles() {
 		return Collections.unmodifiableList(files);
 	}
-	
+
 	public SourceFile get(int rowIndex) {
 		return files.get(rowIndex);
 	}
-	
+
 	@Override
 	public int getRowCount() {
 		return files.size();
 	}
-	
+
 	@Override
 	public int getColumnCount() {
 		return columns.size();
 	}
-	
+
 	@Override
 	public String getColumnName(int columnIndex) {
 		return columns.get(columnIndex);
 	}
-	
+
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
 		return String.class;
 	}
-	
+
 	@Override
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
 		return false;
 	}
-	
+
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		SourceFile rowValue = files.get(rowIndex);
@@ -71,20 +71,20 @@ public class SourceTableModel implements TableModel {
 				throw new IllegalArgumentException("illegal column index " + columnIndex);
 		}
 	}
-	
+
 	@Override
 	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
 		throw new UnsupportedOperationException("use addAll method");
 	}
-	
+
 	@Override
 	public void addTableModelListener(TableModelListener l) {
 		listeners.add(l);
 	}
-	
+
 	@Override
 	public void removeTableModelListener(TableModelListener l) {
 		listeners.remove(l);
 	}
-	
+
 }
--- src/main/java/org/zephyrsoft/albumcreator/model/TargetTableModel.java
+++ src/main/java/org/zephyrsoft/albumcreator/model/TargetTableModel.java
@@ -1,8 +1,8 @@
 package org.zephyrsoft.albumcreator.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.event.TableModelEvent;
@@ -10,18 +10,18 @@ import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
 
 public class TargetTableModel implements TableModel {
-	
+
 	private static final List<String> columns = Arrays.asList("Track", "Artist", "Title");
-	
-	private List<TargetFile> files = new LinkedList<>();
-	private List<TableModelListener> listeners = new LinkedList<>();
-	
+
+	private List<TargetFile> files = new ArrayList<>();
+	private List<TableModelListener> listeners = new ArrayList<>();
+
 	public void add(TargetFile targetFile) {
 		targetFile.setTrackNumber(files.size() + 1);
 		files.add(targetFile);
 		listeners.forEach(listener -> listener.tableChanged(new TableModelEvent(this, files.size() - 1)));
 	}
-	
+
 	public void remove(int rowIndex) {
 		files.remove(rowIndex);
 		// if a track was removed from the middle of the list, we have to renumber all tracks
@@ -30,45 +30,45 @@ public class TargetTableModel implements TableModel {
 		}
 		listeners.forEach(listener -> listener.tableChanged(new TableModelEvent(this, rowIndex)));
 	}
-	
+
 	public void clear() {
 		files.clear();
 		listeners.forEach(listener -> listener.tableChanged(new TableModelEvent(this)));
 	}
-	
+
 	public TargetFile get(int rowIndex) {
 		return files.get(rowIndex);
 	}
-	
+
 	public List<TargetFile> getFiles() {
 		return Collections.unmodifiableList(files);
 	}
-	
+
 	@Override
 	public int getRowCount() {
 		return files.size();
 	}
-	
+
 	@Override
 	public int getColumnCount() {
 		return columns.size();
 	}
-	
+
 	@Override
 	public String getColumnName(int columnIndex) {
 		return columns.get(columnIndex);
 	}
-	
+
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
 		return String.class;
 	}
-	
+
 	@Override
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
 		return false;
 	}
-	
+
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		TargetFile rowValue = files.get(rowIndex);
@@ -83,20 +83,20 @@ public class TargetTableModel implements TableModel {
 				throw new IllegalArgumentException("illegal column index " + columnIndex);
 		}
 	}
-	
+
 	@Override
 	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
 		throw new UnsupportedOperationException("use add and remove methods");
 	}
-	
+
 	@Override
 	public void addTableModelListener(TableModelListener l) {
 		listeners.add(l);
 	}
-	
+
 	@Override
 	public void removeTableModelListener(TableModelListener l) {
 		listeners.remove(l);
 	}
-	
+
 }
