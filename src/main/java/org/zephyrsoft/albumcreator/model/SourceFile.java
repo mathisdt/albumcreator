c0e939ace5097fb64ab318310d8db201cccd94de
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2021-05-27 22:33:16 +0200

    don't cut off text after an extra dash

--- src/main/java/org/zephyrsoft/albumcreator/model/SourceFile.java
+++ src/main/java/org/zephyrsoft/albumcreator/model/SourceFile.java
@@ -8,28 +8,29 @@ public class SourceFile {
 	private final Path path;
 	private final String artist;
 	private final String title;
-	
+
 	public SourceFile(Path path) {
 		this.path = path;
-		
+
 		// parse artist and title
 		String fileName = path.getFileName().toString();
-		artist = removeMusicFileExtension(fileName.replaceAll("\\s++-\\s.*$", ""));
-		title = removeMusicFileExtension(fileName.replaceAll("^.*\\s-\\s++", ""));
+		String[] artistAndTitle = removeMusicFileExtension(fileName).split("\\s++-\\s++", 2);
+		artist = artistAndTitle[0];
+		title = artistAndTitle[1];
 	}
-	
+
 	public Path getPath() {
 		return path;
 	}
-	
+
 	public String getArtist() {
 		return artist;
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -37,7 +38,7 @@ public class SourceFile {
 		result = prime * result + ((path == null) ? 0 : path.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -54,5 +55,5 @@ public class SourceFile {
 			return false;
 		return true;
 	}
-	
+
 }
